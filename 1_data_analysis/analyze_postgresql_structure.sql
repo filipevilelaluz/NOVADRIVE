3f6aad76add158fed570c51ce19fd50a92858c51
Filipe Vilela Luz <74246172+filipevilelaluz@users.noreply.github.com>
2024-07-08 22:24:02 -0300

    Add files via upload

--- 1_data_analysis/analyze_postgresql_structure.sql
+++ 1_data_analysis/analyze_postgresql_structure.sql
@@ -1,1 +1,21 @@
+-- Lista todas as tabelas no banco de dados
+
+SELECT table_name
+FROM information_schema.tables
+WHERE table_schema = 'public';
+
+
+-- Descreve a estrutura de cada tabela, incluindo colunas e seus tipos de dados
+SELECT table_name, column_name, data_type
+FROM information_schema.columns
+WHERE table_schema = 'public'
+ORDER BY table_name, ordinal_position;
+
+
+-- Lista todos os índices no banco de dados
+SELECT indexname, indexdef
+FROM pg_indexes
+WHERE schemaname = 'public';
+
+
 
